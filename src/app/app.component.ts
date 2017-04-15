b75e608a28d69763bae0a8ab2b6a00ba2f795def
Andras Ivacson <Andras Ivacson>
2017-04-16 00:51:32 +0200

    added material design + basic user management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent  {
+  userName = null;
+  uid: string;
+  userList: FirebaseListObservable<any>;
+  invitedUser: string = '';
+
+  constructor(public af: AngularFire) {
+    this.userList = af.database.list('/users', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.af.auth.subscribe(auth => { 
+      if(auth) {
+        this.userName = auth.anonymous ? 'Anonymous': auth.auth.displayName;
+        this.uid = auth.uid;
+      }
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Anonymous,
+      method: AuthMethods.Anonymous
+    });
+  }
+
+  Send(value: string) {
+      this.userList.push({ email: value});
+      this.invitedUser = '';
+  }
+
+  Remove(item: any) {
+    this.af.database.list('/users/' + item.$key).remove();
+    this.invitedUser = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+import {MaterialModule} from '@angular/material';
+
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyB9o47zthwB-oXMmxhY_CYPk1Xkk6rBR1g",
+    authDomain: "portal-8cd0a.firebaseapp.com",
+    databaseURL: "https://portal-8cd0a.firebaseio.com",
+    projectId: "portal-8cd0a",
+    storageBucket: "portal-8cd0a.appspot.com",
+    messagingSenderId: "375358890598"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, AngularFireModule.initializeApp(firebaseConfig),
+    MaterialModule.forRoot()],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
